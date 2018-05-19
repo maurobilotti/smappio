3ab114162958b227bf543a306b872f47a3065aa1
Pablo Furst <furstpablo@gmail.com>
2018-05-19 16:20:51 -0300

    Bluetooth con captura de sonido de micrófono de 3 pines

--- HW/Arduino/others/huzzah_bluetooth/huzzah_bluetooth.ino
+++ HW/Arduino/others/huzzah_bluetooth/huzzah_bluetooth.ino
@@ -6,6 +6,10 @@
 
 #include "BluetoothSerial.h"
 
+const int inputPin = A3;
+unsigned int inputSample;
+const int inputWindow = 100;
+
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
@@ -13,12 +17,21 @@
 BluetoothSerial SerialBT;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  pinMode(inputPin, INPUT);
   SerialBT.begin("smappio3"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  SerialBT.print("hola");
-  delay(20);
+  //SerialBT.print("hola");
+  //delay(20);
+  // loop for the window
+  for (unsigned int i = 0; i < inputWindow; i++) {
+    // read in a single value
+    inputSample = analogRead(inputPin);
+  }
+  
+  SerialBT.print(inputSample);
+  
 }
