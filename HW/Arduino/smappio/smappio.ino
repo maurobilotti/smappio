b129733a9a34833b91b51ee5a3de14a2f2f14781
augustobonabia <augusto.bonabia@gmail.com>
2018-08-05 04:52:16 -0300

    - Se limpia y organiza el codigo del sketch para puerto Serial.

--- HW/Arduino/smappio/smappio.ino
+++ HW/Arduino/smappio/smappio.ino
@@ -2,14 +2,12 @@
 
 // CONSTANTES
 #define SAMPLES_TO_SEND 3
+#define MEDIA 13700   // I2S:  13700  | PCM:  6835   // valor para nivelar a 0 la señal media
 
 // VARIABLES
-int32_t *buffer;
-int32_t value = 0;
-int bytesReaded = 0;
-int media = 13700;  // I2S:  13700  | PCM:  6835   // valor para nivelar a 0 la señal media
-uint8_t dataToSend[SAMPLES_TO_SEND];
-SmappioSound smappioSound(media); 
+int32_t *_buffer;
+uint8_t _dataToSend[SAMPLES_TO_SEND];
+SmappioSound smappioSound(MEDIA); 
 
 void setup() {
   pinMode(DATA_PIN, INPUT); // Supuestamente necesario para que no haya ruido
@@ -18,7 +16,7 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  smappioSound.begin(buffer);
+  smappioSound.begin(_buffer);
 }
 
 void loop() 
@@ -28,31 +26,34 @@ void loop()
     char c = (char)Serial.read();
     if(c == 's') // Si el cliente mando la señal send ('s'), se empieza a transmitir, caso contrario, ignora dicha señal
     {  
-      while(1) { // Se queda transmitiendo por siempre
+      while(1) 
+      { // Se queda transmitiendo por siempre, hasta que se aprete el boton
         bufferSamplesToSend();
-        Serial.write(dataToSend, sizeof(dataToSend));   
+        Serial.write(_dataToSend, sizeof(_dataToSend));
       }
     }
   }
 }
 
+
 void bufferSamplesToSend() 
 {
-    for(int i = 0; i < SAMPLES_TO_SEND; i += 3)
-    {
-      // Se hace la lectura de los samples del microfono
-      bytesReaded = smappioSound.read();
-      while(bytesReaded == 0)
-      {     
-          //Este bucle es necesario para no reenviar una muestra mas de una vez
-          bytesReaded = smappioSound.read();
-      }   
-      // Se lee un sample
-      value = smappioSound.getSampleValue();
-      dataToSend[i] = value & 255;
-      dataToSend[i + 1] = (value >> 8)  & 255;
-      dataToSend[i + 2] = (value >> 16) & 255; 
-    }  
+  int32_t value = 0;
+  int bytesReaded = 0;
+
+  for(int i = 0; i < SAMPLES_TO_SEND; i += 3)
+  {
+    // Se hace la lectura de los samples del microfono
+    bytesReaded = smappioSound.read();
+    while(bytesReaded == 0)
+    {     
+        //Este bucle es necesario para no reenviar una muestra mas de una vez
+        bytesReaded = smappioSound.read();
+    }   
+    // Se lee un sample
+    value = smappioSound.getSampleValue();
+    _dataToSend[i] = value & 255;
+    _dataToSend[i + 1] = (value >> 8)  & 255;
+    _dataToSend[i + 2] = (value >> 16) & 255; 
+  }  
 }
-
-
