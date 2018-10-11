6e8483f8abc4c46331b236106f263ee451b44331
nicmora <nicmora24@gmail.com>
2018-10-11 14:42:59 -0300

    Agregando flush para limpiar la memoria de audiotrack, acomodando el codigo

--- SW/Smappio/app/src/main/java/ar/com/smappio/PCMSocket.java
+++ SW/Smappio/app/src/main/java/ar/com/smappio/PCMSocket.java
@@ -42,7 +42,7 @@ public class PCMSocket {
                 e.printStackTrace();
             }
 
-            while(isPlaying) {
+            while (isPlaying) {
 
                 try {
                     InputStream is = socket.getInputStream();
@@ -62,7 +62,9 @@ public class PCMSocket {
 
                 playIfNeccesary();
             }
+
             audioTrack.stop();
+            audioTrack.flush();
 
             try {
                 socket.close();
@@ -75,14 +77,11 @@ public class PCMSocket {
     /**
      * Le da Play la pista de audio solo si se recibieron "prebufferingSize" veces "playinLengt" bytes de datos.
      */
-    private void playIfNeccesary()
-    {
+    private void playIfNeccesary() {
         if (prebufferingCounter > prebufferingSize) {
             audioTrack.play();
             prebufferingCounter = -1; // Se sale de la etapa de prebuffereo
-        }
-        else if (prebufferingCounter != -1)
-        {
+        } else if (prebufferingCounter != -1) {
             prebufferingCounter++;
         }
     }
@@ -90,7 +89,7 @@ public class PCMSocket {
     private float[] getBufferForPlaying(byte[] buffer) {
         float[] returnedArray = new float[playingLength / 3];
         for (int i = 0; i < buffer.length; i = i + 3) {
-            byte signBit = (byte)((buffer[i + 2] >> 1) & 1);
+            byte signBit = (byte) ((buffer[i + 2] >> 1) & 1);
 
             int intValue = ((buffer[i] & 0xFF) << 0)
                     | ((buffer[i + 1] & 0xFF) << 8)
@@ -104,48 +103,39 @@ public class PCMSocket {
         return returnedArray;
     }
 
-    private byte[] controlAlgorithm()
-    {
+    private byte[] controlAlgorithm() {
         // Verificar que lo que se lee cumpla con la secuencia 01, 10, 11
         byte[] errorFreeBuffer = new byte[playingLength];
         int i = 0;
         int acumDiscardedBytes = 0;
-        while (i < readedAux - 2)
-        {
-            int firstByteSeqNumber = (bufferAux[i] >> 6)  & 3;
+        while (i < readedAux - 2) {
+            int firstByteSeqNumber = (bufferAux[i] >> 6) & 3;
             int secondByteSeqNumber = (bufferAux[i + 1] >> 6) & 3;
             int thirdByteSeqNumber = (bufferAux[i + 2] >> 6) & 3;
             int discardedBytes = 0;
 
             // Si algun numero no no sigue la secuencia, se descartan bytes para atras, nunca para delante
-            if (firstByteSeqNumber != 1)
-            {
+            if (firstByteSeqNumber != 1) {
                 discardedBytes += 1;
-            }
-            else if (secondByteSeqNumber != 2)
-            {
-                if (secondByteSeqNumber == 0)
-                {
+            } else if (secondByteSeqNumber != 2) {
+                if (secondByteSeqNumber == 0) {
+                    discardedBytes += 1;
+                } else if (secondByteSeqNumber == 1) {
                     discardedBytes += 1;
                 }
-                else if (secondByteSeqNumber == 1)
-                    discardedBytes += 1;
-                else if (secondByteSeqNumber == 3)
+                else if (secondByteSeqNumber == 3) {
                     discardedBytes += 2;
-            }
-            else if (thirdByteSeqNumber != 3)
-            {
-                if (thirdByteSeqNumber == 0)
-                {
-                    discardedBytes += 1;
                 }
-                else if (thirdByteSeqNumber == 1)
+            } else if (thirdByteSeqNumber != 3) {
+                if (thirdByteSeqNumber == 0) {
+                    discardedBytes += 1;
+                } else if (thirdByteSeqNumber == 1) {
                     discardedBytes += 2;
-                else if (thirdByteSeqNumber == 2)
+                }
+                else if (thirdByteSeqNumber == 2) {
                     discardedBytes += 3;
-            }
-            else
-            {
+                }
+            } else {
                 // Se vuelven a armar las muestras originales
                 int sample = 0;
                 byte[] sampleAsByteArray = new byte[4];
@@ -153,28 +143,25 @@ public class PCMSocket {
                 byte auxByteMSB = 0;    // Most Significant Bits
 
                 // Byte 1 => ultimos 6 bits del primer byte + 2 últimos bits del segundo byte
-                auxByteMSB = (byte)((bufferAux[i + 1] & 3) << 6);                           // 'XX|000000'
-                sampleAsByteArray[0] = (byte)(auxByteMSB | (bufferAux[i] & 63));            // 'XX|YYYYYY'
+                auxByteMSB = (byte) ((bufferAux[i + 1] & 3) << 6);                           // 'XX|000000'
+                sampleAsByteArray[0] = (byte) (auxByteMSB | (bufferAux[i] & 63));            // 'XX|YYYYYY'
 
                 // Byte 2 => 4 bits del medio del segundo byte + 4 úlitmos bits del último byte
-                auxByteMSB = (byte)((bufferAux[i + 2] & 15) << 4);                          // 'XXXX|0000'
-                sampleAsByteArray[1] = (byte)(auxByteMSB | ((bufferAux[i + 1] >> 2) & 15)); // 'XXXX|YYYY'
+                auxByteMSB = (byte) ((bufferAux[i + 2] & 15) << 4);                          // 'XXXX|0000'
+                sampleAsByteArray[1] = (byte) (auxByteMSB | ((bufferAux[i + 1] >> 2) & 15)); // 'XXXX|YYYY'
 
                 // Byte 3 => 1 bit (el 4to de izq a derecha)
-                sampleAsByteArray[2] = (byte)((bufferAux[i + 2] >> 4) & 1);                 // '0000000|X'
+                sampleAsByteArray[2] = (byte) ((bufferAux[i + 2] >> 4) & 1);                 // '0000000|X'
 
                 // Byte 3 => 5 bits para el signo(depende del 3ero de izq a derecha)
                 // Si el bit mas significativo del samlpe es '1' quiere decir que el numero es negativo, entonces se
                 // agrega un padding a la izquierda de '7 + 8' unos, caso contrario, se deja el padding 0 que ya habia y se agregan '8' ceros mas
-                byte signBit = (byte)((bufferAux[i + 2] >> 5) & 1);
-                if (signBit == 1)
-                {
-                    sampleAsByteArray[2] = (byte)(sampleAsByteArray[2] | 254);              // '1111111|X'
-                    sampleAsByteArray[3] = (byte)255;                                             // '11111111'
-                }
-                else
-                {
-                    sampleAsByteArray[3] = 0;                                               // '00000000'
+                byte signBit = (byte) ((bufferAux[i + 2] >> 5) & 1);
+                if (signBit == 1) {
+                    sampleAsByteArray[2] = (byte) (sampleAsByteArray[2] | 254);              // '1111111|X'
+                    sampleAsByteArray[3] = (byte) 255;                                       // '11111111'
+                } else {
+                    sampleAsByteArray[3] = 0;                                                // '00000000'
                 }
 
                 errorFreeBuffer[errorFreeBaseIndex] = sampleAsByteArray[0];
@@ -182,10 +169,10 @@ public class PCMSocket {
                 errorFreeBuffer[errorFreeBaseIndex + 2] = sampleAsByteArray[2];
             }
 
-            if (discardedBytes == 0)
+            if (discardedBytes == 0) {
                 i += 3;
-            else
-            {
+            }
+            else {
                 i += discardedBytes;
                 acumDiscardedBytes += discardedBytes;
                 readExtraBytes(discardedBytes);
@@ -195,12 +182,10 @@ public class PCMSocket {
         return errorFreeBuffer;
     }
 
-    private void readExtraBytes(int size)
-    {
-
+    private void readExtraBytes(int size) {
         try {
             InputStream is = socket.getInputStream();
-            while(is.available() < size) {
+            while (is.available() < size) {
                 // do nothing
             }
             readedAux += is.read(bufferAux, readedAux, size);
