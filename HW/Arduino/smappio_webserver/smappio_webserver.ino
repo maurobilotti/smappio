5d1c0fe680dea09f0250993cd61e02106cfeac56
augustobonabia <augusto.bonabia@gmail.com>
2018-12-05 00:54:37 -0300

    arreglo logger

--- HW/Arduino/smappio_webserver/smappio_webserver.ino
+++ HW/Arduino/smappio_webserver/smappio_webserver.ino
@@ -29,7 +29,7 @@ const char* password = "123456789"; // El pass tiene que tener mas de 8 caracter
 #define SMAPPIO_CODE_TEST 2
 #define SMAPPIO_CODE_LOGS 3
 
-#define LOGGER_SIZE 10
+#define LOGGER_SIZE 100
 
 enum logAudit {
   ON, // 0
@@ -140,8 +140,9 @@ void loop() {
       }
       else if(code == SMAPPIO_CODE_LOGS)
       {
-        if(client.connected())
+        while(client.connected())
         {
+          deepSleepIfButtonPressed();
           int bytesSizeToSend = bufferLogsToSend();
           client.write(_dataToSend, bytesSizeToSend);
         }
